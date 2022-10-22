b5431b9b677498e1cdb5d5b2b85df80697902c65
tipanyya06 <116444100+tipanyya06@users.noreply.github.com>
2022-10-22 22:26:12 +0800

    Add files via upload

---
+++ myfirstjavajava.java
@@ -0,0 +1,17 @@
+public class myfirstjavajava {
+        public static void main(String args[]){ 
+    
+            int X=5, Y=3, Z=0; 
+    
+            X = Z; 
+            Y = X; 
+            X = 3; 
+            Z = 7 - X; 
+            Y = Z; 
+            X = Z + Y; 
+    
+            System.out.println("ANG TAMANG SAGOT AY: " + Z); 
+            System.out.println("ANG TAMANG SAGOT AY: " + X); 
+
+}
+}
---
+++ myfirstjavaname.java
@@ -0,0 +1,6 @@
+public class myfirstjavaname {
+    public static void main(String args[]){
+
+        System.out.println(" HELLO WORLD");
+    }
+}
